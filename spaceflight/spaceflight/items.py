44e70383cf49b706f141c596fc720b40d903c259
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-09-24 22:41:47 +0300

    crawler item

--- spaceflight/spaceflight/items.py
+++ spaceflight/spaceflight/items.py
@@ -1,12 +1,11 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class SpaceflightItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    organization = scrapy.Field()
+    location = scrapy.Field()
+    date = scrapy.Field()
+    detail = scrapy.Field()
+    rocket_status = scrapy.Field()
+    price = scrapy.Field()
+    mission_status = scrapy.Field()
