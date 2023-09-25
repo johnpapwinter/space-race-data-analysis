3ef67a9f26c39bf6bee7a1356c9242ca01bdc395
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-09-25 23:39:32 +0300

    some preliminary scraping

--- spaceflight/spaceflight/spiders/nextspaceflight.py
+++ spaceflight/spaceflight/spiders/nextspaceflight.py
@@ -1,4 +1,5 @@
 import scrapy
+from spaceflight.spaceflight.items import SpaceflightItem
 
 
 class NextspaceflightSpider(scrapy.Spider):
@@ -7,4 +8,19 @@ class NextspaceflightSpider(scrapy.Spider):
     start_urls = ["https://nextspaceflight.com/launches/past/?page=1"]
 
     def parse(self, response):
-        pass
+        launches = response.css('div.mdl-grid div.mdl-cell.mdl-cell--6-col')
+
+        for launch in launches:
+            yield from self.parse_launch(launch)
+
+    def parse_launch(self, response):
+        launch_item = SpaceflightItem()
+        launch_item['organization'] = response.css('div.launch div.mdl-card__title-text span::text').get()
+        launch_item['location'] = None
+        launch_item['date'] = None
+        launch_item['detail'] = response.css('div.launch h5::text').get()
+        launch_item['rocket_status'] = None
+        launch_item['price'] = None
+        launch_item['mission_status'] = None
+
+        yield launch_item
