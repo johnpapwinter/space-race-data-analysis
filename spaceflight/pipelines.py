d652f1928114e13966ca210eeb4b686d417c7bd4
johnpapwinter <johnpapahimonas@yahoo.com>
2023-09-28 14:16:22 +0300

    fixes in pipeline for datetime and cost

--- spaceflight/pipelines.py
+++ spaceflight/pipelines.py
@@ -1,11 +1,18 @@
 from itemadapter import ItemAdapter
+from datetime import datetime
 
 
 class SpaceflightPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
         field_names = adapter.field_names()
+
+        date_format = '%a %b %d, %Y %H:%M %Z'
+
         for field_name in field_names:
+            if field_name == 'date':
+                value = adapter.get(field_name)
+                adapter[field_name] = datetime.strptime(value, date_format)
             if field_name == 'rocket_name':
                 value = adapter.get(field_name)
                 adapter[field_name] = value.replace('\n', '').strip()
@@ -18,7 +25,7 @@ class SpaceflightPipeline:
             if field_name == 'price':
                 value = adapter.get(field_name)
                 if 'Price:' in value:
-                    adapter[field_name] = value.replace('Price:', '').strip()
+                    adapter[field_name] = value.replace('Price: $', '').replace("million", "").strip()
                 else:
                     adapter[field_name] = None
 
