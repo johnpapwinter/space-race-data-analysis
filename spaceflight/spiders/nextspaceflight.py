cc13aab173868e06b3784dffd2f6dc9c69ec1ee4
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-09-29 22:32:08 +0300

    removed commented-out code

--- spaceflight/spiders/nextspaceflight.py
+++ spaceflight/spiders/nextspaceflight.py
@@ -29,7 +29,6 @@ class NextspaceflightSpider(scrapy.Spider):
         launch_item['mission_name'] = response.css('section div.mdl-cell div.mdl-card__supporting-text h4::text').get()
         launch_item['location'] = (
             response.xpath('//h3[text()="Location"]').xpath('following-sibling::section')[0].xpath('.//h4/text()').get())
-            # response.css('section.card div.mdl-card__supporting-text h4.mdl-card__title-text::text')[2].get())
         launch_item['date'] = (
             response.css('section div.mdl-cell div.mdl-card__supporting-text div.mdl-grid.a div.mdl-cell span::text').get())
         launch_item['rocket_name'] = (
